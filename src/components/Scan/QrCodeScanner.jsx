3ac19aff1de40aaba1e8e8e6bafedf530e31e2c6
Nikita Malinovsky <nikitamalinovsky3@gmail.com>
2025-04-06 10:29:21 +0300

    fix: qrReader

--- src/components/Scan/QrCodeScanner.jsx
+++ src/components/Scan/QrCodeScanner.jsx
@@ -25,6 +25,7 @@ export const QrCodeScanner = () => {
     return (
         <div className={s.container}>
             <QrReader
+                constraints={{ facingMode: 'environment' }}
                 scanDelay={1000}
                 onResult={scanHandler}
                 containerStyle={{width: '200px'}}
